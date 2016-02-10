344e29bee5fd736273060aa5e773739c43604d8d
Kalaja Eero M A <ekalaja@da5-cs-bk107-37.helsinki.fi>
2016-02-10 17:19:30 +0200

    cardlayout kayttoon

--- Lautapelikello/src/main/java/fi/ekalaja/boardgameclock/clockui/SwingUi.java
+++ Lautapelikello/src/main/java/fi/ekalaja/boardgameclock/clockui/SwingUi.java
@@ -3,21 +3,33 @@ package fi.ekalaja.boardgameclock.clockui;
 import fi.ekalaja.boardgameclock.SimpleTimer;
 import fi.ekalaja.boardgameclock.TimeLogic;
 import fi.ekalaja.boardgameclock.actionlistener.PlayersActionListener;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-public class SwingUi implements Runnable {
 
+public class SwingUi implements Runnable, ItemListener {
+
+//    private JPanel cardLayoutPanel;
+//    private JPanel card1;
     private JFrame frame;
     private ArrayList<SimpleTimer> allClocks;
     private TimeLogic timelogic;
+    private JPanel cards;
+    final static String CLOCKSPANEL = "CLOCKS";
+    final static String SETUPPANEL = "SETUP";
 
     public SwingUi(ArrayList<SimpleTimer> allClocks, TimeLogic timelogic) {
         this.allClocks = allClocks;
@@ -25,18 +37,32 @@ public class SwingUi implements Runnable {
     }
 
     @Override
-    public void run() {
+    public void run() {       
+
         frame = new JFrame("Clocks");
         frame.setPreferredSize(new Dimension(600, 200));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.createAllComponents();
-        // käytä frame.getContentPane() allComponents sisällä
+        this.createAllComponents(frame.getContentPane());
         frame.pack();
         frame.setVisible(true);
+        
+//        cardLayoutPanel.add(frame);
     }
 
-    public void createAllComponents() {
-        frame.setLayout(new GridLayout(2, 1));
+    public void createAllComponents(Container pane) {
+
+        JPanel comboBoxPane = new JPanel();
+        String comboBoxItems[]  = { CLOCKSPANEL, SETUPPANEL};
+        JComboBox cb = new JComboBox(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+//        cb.addItemListener(null);
+        comboBoxPane.add(cb);
+        JPanel card1 = new JPanel();
+        // lisätään setup toiminnallisuudet card1:seen.
+        
+        
+        JPanel card2 = new JPanel(new GridLayout(2,1));
         JPanel panelOfClocks = new JPanel(new GridLayout(1, 0));
 
         for (int i = 0; i < this.allClocks.size(); i++) {
@@ -47,10 +73,7 @@ public class SwingUi implements Runnable {
             ClockFrame.setEnabled(false);
             panelOfClocks.add(ClockFrame);
         }
-//        ClockFrame.setForeground(Color.GREEN); ei toimi noin
-//        panel.setFont(new Font("Greek", 1, 30)); eikä näin
-        frame.add(panelOfClocks);
-
+        
         JButton start = new JButton("Start/Pause");
         JButton next = new JButton("Next");
         start.setName("Start/Pause");
@@ -59,16 +82,35 @@ public class SwingUi implements Runnable {
         PlayersActionListener userListener = new PlayersActionListener(next, start, timelogic);
         next.addActionListener(userListener);
         start.addActionListener(userListener);
-
+        
         JPanel panelOfButtons = new JPanel(new GridLayout(1, 0));
         panelOfButtons.add(next);
         panelOfButtons.add(start);
-        frame.add(panelOfButtons);
+        
+        card2.add(panelOfButtons);
+        card2.add(panelOfClocks);
+//        frame.add(panelOfButtons);
+        
+        
+        cards = new JPanel(new CardLayout());
+        cards.add(card2, CLOCKSPANEL);
+        cards.add(card1, SETUPPANEL);
+        
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+//        frame.setLayout(new GridLayout(2, 1)); oli ennen
+//        frame.add(panelOfClocks);
 
+    }
+    
+    public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
     }
 
     public JFrame getFrame() {
         return frame;
     }
+    
 
 }
